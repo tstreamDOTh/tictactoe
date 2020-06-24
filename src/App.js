e44b2e3198eef7af20c2e966ccf8803131d9c44f
tstreamDOTh <tstream.h@outlook.com>
2020-06-24 17:01:41 +0530

    Setup basic mobx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Board } from './components/board/Board';
 import './App.scss';
 import { Provider, Observer } from 'mobx-react';
@@ -7,13 +6,11 @@ import Game from './models/game';
 
 const game = new Game(4);
 
-const ObserverBoard = Observer(Board);
-
 function App() {
   return (
     <Provider game={game}>
       <div>
-        <ObserverBoard />
+        <Board />
       </div>
     </Provider>
   );
--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './styles.scss';
+import { observer, inject } from 'mobx-react';
 
-export const Board = ({ game }) => {
-  const { board } = game;
-  return (
-    <div className={'board'}>
-      {board.map((row) => (
-        <div className={'row'}>
-          {row.map((cell) => (
-            <div className={`cell ${cell}`}> {cell} </div>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+export const Board = inject('game')(
+  observer(({ game }) => {
+    const { board } = game;
+    return (
+      <div className={'board'}>
+        {board.map((row) => (
+          <div className={'row'}>
+            {row.map((cell) => (
+              <div className={`cell ${cell}`}> {cell} </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  })
+);
